7fd1bc4702292cc568ccf66ff9b69308227a45f3
ums-L200220212 <l200220212@student.ums.ac.id>
2024-10-26 18:58:43 +0700

    added via vs code

---
+++ kuliahInformatikaUMS.py
@@ -0,0 +1,8 @@
+print("""bayar spp sebelum waktu pengambilan mata kuliah habis\nsetelah bayar, 
+ambil mata kuliah yang ingin diambil,
+\ntertib mengikuti kelas mata kuliah dan mengerjakan tugas yang diberikan,
+\nmengikuti uts yang akan diadakan,
+\nbayar spp untuk 3 bulan kedepan,
+\nmengikuti kelas dan mengerjakan tugas akhir jika ada,
+\nmengikuti uas yang akan diadakan,
+\nmenunggu nilai tercetak di star ums.""")
